e0db2f8b02da55f9329edfa6c63898ff5016c400
Lambert-Simnel <Josephsut@gmail.com>
2017-05-27 16:27:24 +0100

    Create scales.js
    Just base scales? Hopefully not fucked up?

---
+++ scales.js
@@ -0,0 +1,22 @@
+//Well here goes nothing I guess
+const ids = require("../../MrHound/Current/ids.js");
+const dice = require("../../MrHound/Current/dice.js");
+const event = require("../../MrHound/Current/emitter.js");
+const rw = require("../../MrHound/Current/reader_writer.js");
+
+
+module.exports =
+{
+    var HEATSCALE = -COLDSCALE
+    var COLDSCALE = -HEATSCALE
+    var ORDERSCALE = -TURMOILSCALE
+    var TURMOILSCALE = -ORDERSCALE
+    var PRODSCALE = -SLOTHSCALE
+    var SLOTHSCALE = -PRODSCALE
+    var GROWTHSCALE = -DEATHSCALE
+    var DEATHSCALE = -GROWTHSCALE
+    var LUCKSCALE = -UNLUCKSCALE
+    var UNLUCKSCALE = -LUCKSCALE
+    var MAGICSCALE = -DRAINSCALE
+    var DDRAINSCALE = -MAGICSCALE
+}    
